ea866fac3aa3b5269f0b311bc490f05ef7d70a0d
nrahaman <nasim.rahaman@iwr.uni-heidelberg.de>
2016-10-06 19:29:58 +0200

    - finished experience replay

--- Boilerplate/tools.py
+++ Boilerplate/tools.py
@@ -1,20 +1,41 @@
 __author__ = "nasim.rahaman (at) iwr.uni-heidelberg.de"
 
+import random
+
 
 class ExperienceDatabase(object):
     """A simple list like class with a few bells and whistles to implement experience replay for Deep Q-Learning."""
-    def __init__(self):
-        pass
+    def __init__(self, maxsize=None):
+        """
+        :type maxsize: int
+        :param maxsize: Maximum size of the database.
+        """
+        # Meta
+        self.maxsize = maxsize
+        # Database, implemented as a simple list
+        self.db = []
 
-    def sample(self):
+    def __len__(self):
+        return len(self.db)
+
+    def sample(self, idx=None):
         """Sample experience from database."""
-        pass
+        # Get index of the experience to sample
+        if idx is None:
+            idx = random.randint(0, len(self) - 1)
+        # Sample
+        experience = self.db[idx]
+        # Return
+        return experience
 
     def record(self, *experience):
         """Record an experience to the database."""
-        # Remember to pop if necessarily.
-        pass
+        # Pop if full.
+        if len(self) > self.maxsize:
+            self.db.pop(0)
+        # Record experience
+        self.db.append(experience)
 
     def save(self):
         """Saves a copy of itself to disk."""
-        pass
+        raise NotImplementedError
